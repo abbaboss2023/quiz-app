6a6052974af12eefd1839760f7eb2da78391f16c
abbaboss2023 <abbaboss2023@gmail.com>
2023-12-05 11:08:40 +0100

    my first project js

---
+++ js/script.js
@@ -0,0 +1,146 @@
+const questions = [
+    {
+        questions: "Which is larget animal in the world",
+        answers: [
+            { text: "Shark", correct: false},
+            { text: "Blue Whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Graffe", correct: false},
+        ]
+    },
+    
+    {
+        questions: "Which is the smallest country in the world",
+        answers: [
+            { text: "Vatican City", correct: true },
+            { text: "Bhutan", correct: false },
+            { text: "Nepal", correct: false },
+            { text: "Shari Lanka", correct: false },
+        ]
+    },
+
+    {
+        questions: "Which is the largest desert in the world",
+        answers: [
+            { text: "Kalahari", correct: false },
+            { text: "Gobi", correct: false },
+            { text: "Sahara", correct: false },
+            { text: "Antactica", correct: true },
+        ]
+    },
+
+    {
+        questions: "Which is the smallest continate in the world",
+        answers: [
+            { text: "Asia", correct: false },
+            { text: "Australia", correct: true },
+            { text: "Arctic", correct: false },
+            { text: "Africa", correct: false },
+        ]
+    },
+
+     {
+        questions: "Which is the smallest continate in the world",
+        answers: [
+            { text: "Asia", correct: false },
+            { text: "Australia", correct: true },
+            { text: "Arctic", correct: false },
+            { text: "Africa", correct: false },
+        ]
+    }
+]
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+ 
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let QuestionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = QuestionNo + ". " + currentQuestion.questions;
+
+    currentQuestion.answers.forEach(answer => {
+            const button = document.createElement("button");
+            button.innerHTML = answer.text;
+            button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+            button.addEventListener("click", selectAnswer);
+        });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true"){
+            button.classList.add("correct");
+        } 
+        button.disabled = "true";
+    });
+    nextButton.style.display = "Block";
+    
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    }else{
+        showScore();  
+    }
+}
+
+nextButton.addEventListener("click",  ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else {
+        startQuiz();
+    }
+});
+
+startQuiz();
+
+ 
+
+//=========button=======
+var greetinginnerText = document.getElementById(); 
+function changeText() {
+    
+    alert("Thank you for click");
+}  
+
